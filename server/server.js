baa11c5c4005e5ec256afa2176b19a66567a8cac
Karolis Kerpys <karolis.kerpys@gmail.com>
2017-12-22 10:18:08 +0200

    socket.io connection

--- server/server.js
+++ server/server.js
@@ -12,6 +12,15 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket)=>{
+  console.log('New user connected');
+  socket.on('disconnect', ()=>{
+    console.log('User disconected from the server')
+  })
+});
+
+
+
 server.listen(port, ()=>{
   console.log(`listening on port ${3000}`)
 });
